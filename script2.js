80b331dfd52410325d2e08bb831dcaa6bba01f90
naufalrizqullah23 <naufalrizqullah23@gmail.com>
2022-01-13 10:24:08 +0700

    konsep scope

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 //2.1 execution context, hoisting, dan scope
-console.log(name);
-var name = 'Naufal Rizqullah';
+// console.log(name);
+// var name = 'Naufal Rizqullah';
 
 //creation phase pada Global Context
 //nama var = undefined
@@ -15,9 +15,43 @@ var name = 'Naufal Rizqullah';
 
 //execution phase
 
-var nama = 'Sinopal';
-var umur = 20;
-console.log(sapa());
-function sapa(){
-    return(`Halo ${nama} yang berumur ${umur} tahun! apa kabs?`);
+// var nama = 'Sinopal';
+// var umur = 20;
+// console.log(sapa());
+// function sapa(){
+//     return(`Halo ${nama} yang berumur ${umur} tahun! apa kabs?`);
+// }
+
+
+//function membuat local Execution Context
+// yang didalamnya terdapat creation dan execution phase
+//window
+//arguments
+//hoisting
+
+//---konsep scope
+var nama = 'Naufal Rizqulah';
+var ig = 'naufalrizqullah23';
+
+function cetakURL(ig){
+    var instaURL = 'https://instagram.com/';
+    return instaURL+ig;
 }
+
+console.log(cetakURL('himatifuinbandung'));
+//konsep scope---
+
+// function a(){
+//     console.log(`ini a`);
+
+//     function b(){
+//         console.log(`ini b`);
+
+//         function c(){
+//             console.log(`ini c`);
+//         }
+//         c();
+//     }
+//     b();
+// }
+// a();
