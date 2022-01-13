4e3315743a529546ce6013e3736fb2deb588e35e
naufalrizqullah23 <naufalrizqullah23@gmail.com>
2022-01-13 11:25:47 +0700

    closure sebagai private method

--- script3.js
+++ script3.js
@@ -2,6 +2,7 @@
 
 //lexical scope
 //----untuk function factories
+//case 1
 function init(){
     // let nama = 'Naufal';
     return function(nama){
@@ -11,11 +12,8 @@ function init(){
 let panggileNama = init();
 panggileNama('Sinopal');
 panggileNama('Rijkuloh');
-//untuk function factories---
 
-
-//untuk private method
-
+//case 2
 function ucapSalam(waktu){
     return function(name){
         console.log(`Selamat ${waktu} ${name}! semoga tuhan bersamamu`);
@@ -29,3 +27,28 @@ let waktuMalam = ucapSalam('malam');
 waktuPagi('Naufal Rizqullah');
 waktuSiang('Naufal Rizqullah');
 waktuPagi('Ajax');
+
+//untuk function factories---
+
+//---untuk private method
+//dengan adanya private method, variable dalam method tersebut bisa tidak terganggu, walaupun diluar variablenya di inisiasi ulang
+let add = (function(){
+    let counter = 0;
+    return function(){
+
+        return ++counter;
+    }
+})();
+//agar dapat dijalankan tanpa harus dimasukin lagi, maka...
+//... function luar dibungkus dengan immmediately invoke function dengan kurung dan ditutup dengan kurung lagi
+// let a = add();
+
+counter = 100;
+
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+
+//untuk private method---
